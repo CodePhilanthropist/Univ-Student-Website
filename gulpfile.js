3beb8aa109616636b17ad8801a7054cec378b524
sampe <barrigaete@gmail.com>
2021-09-27 16:55:24 +0800

    just adding something usable

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
-var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
- 
-gulp.task('inline', function() {
-    return gulp.src('*.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('build/'));
+var gulp = require("gulp"), smoosher = require("gulp-smoosher"),
+  inlineCss = require("gulp-inline-css");
+
+gulp.task("inline", function () {
+  return gulp
+    .src("index.html")
+    
+    .pipe(
+      inlineCss({
+        preserveMediaQueries: true,
+        applyLinkTags: true,
+        removeStyleTags: true,
+      })
+    )
+    .pipe(gulp.dest("dist/"));
 });
+
+// var gulp = require("gulp"),
+//   smoosher = require("gulp-smoosher"),
+//   inlineCss = require("gulp-inline-css");
+
+// gulp.task("default", function () {
+//   return gulp
+//     .src("index.html")
+//     .pipe(smoosher().pipe(inlineCss({
+//                 applyStyleTags: true,
+//                 preserveMediaQueries: true,
+//                 applyLinkTags: true,
+//                 removeStyleTags: true,
+//                 removeLinkTags: true,
+//               })).pipe(gulp.dest("dist/")));
+// });
